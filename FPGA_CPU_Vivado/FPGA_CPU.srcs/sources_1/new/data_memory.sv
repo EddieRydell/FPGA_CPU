edd922bc935a47dc3c4ce441adde58dcb6739819
EddieRydell <eddierydell@gmail.com>
2024-10-11 16:58:59 -0600

    Began large refactor of CPU/OS structure

--- FPGA_CPU_Vivado/FPGA_CPU.srcs/sources_1/new/data_memory.sv
+++ FPGA_CPU_Vivado/FPGA_CPU.srcs/sources_1/new/data_memory.sv
@@ -1,11 +1,20 @@
 `timescale 1ns / 1ps
 
 module data_memory #(parameter MEMORY_DEPTH = 1024) (
-    input logic[31:0] address,
-    output logic[31:0] data
+    output logic [31:0] data_accessed,
+    input logic [31:0] data_to_write,
+    input logic [31:0] access_address,
+    input logic read_enable,
+    input logic write_enable,
+    input logic clk
     );
     logic[31:0] data_memory[0:MEMORY_DEPTH - 1];
     
-    assign data = data_memory[address];
+    always_ff @(posedge clk) begin
+        if (read_enable)
+            data_accessed <= data_memory[access_address];
+        if (write_enable)
+            data_memory[access_address] <= data_to_write;
+    end
     
 endmodule
