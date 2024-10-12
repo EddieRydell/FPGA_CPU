001a4c68b121b0aa1bcf03d0a538fc5f307b6d14
EddieRydell <eddierydell@gmail.com>
2024-10-11 23:25:34 -0600

    continued implementing FPG logic

---
+++ FPGA_CPU_Vivado/FPGA_CPU.gen/sources_1/bd/design_1/hdl/design_1_wrapper.v
@@ -0,0 +1,19 @@
+//Copyright 1986-2022 Xilinx, Inc. All Rights Reserved.
+//Copyright 2022-2024 Advanced Micro Devices, Inc. All Rights Reserved.
+//--------------------------------------------------------------------------------
+//Tool Version: Vivado v.2024.1 (win64) Build 5076996 Wed May 22 18:37:14 MDT 2024
+//Date        : Fri Oct 11 23:20:22 2024
+//Host        : EddieRydell running 64-bit major release  (build 9200)
+//Command     : generate_target design_1_wrapper.bd
+//Design      : design_1_wrapper
+//Purpose     : IP block netlist
+//--------------------------------------------------------------------------------
+`timescale 1 ps / 1 ps
+
+module design_1_wrapper
+   ();
+
+
+  design_1 design_1_i
+       ();
+endmodule
